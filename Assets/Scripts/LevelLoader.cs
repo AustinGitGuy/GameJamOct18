58a5a36d4c50f3001099676191472edd1f4ff702
Jacob <jacob.biederman@mymail.champlain.edu>
2018-10-26 16:54:54 -0400

    Set up Game Scene and Scripts

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+enum Scenes
+{
+    GAME = 0,
+    START = 1,
+    MENU = 2,
+    LEVEL_ONE = 3,
+}
+public class LevelLoader : MonoBehaviour {
+    int currentLevel; //currently loaded level.
+    int nextLevel; //next level to load.
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void loadNextLevel()
+    {
+
+    }
+
+
+}
