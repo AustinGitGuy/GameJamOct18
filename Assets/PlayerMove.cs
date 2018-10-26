b732f98178fcfcf940b604dfaa652690f267be64
Austin <austin.laimos@mymail.champlain.edu>
2018-10-26 14:21:29 -0400

    Added some player movement and pot pushing.

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    Rigidbody2D rb;
+    float xMove, yMove;
+    float moveMod = 5f;
+	void Start(){
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update(){
+        GetInput();
+        Move();
+	}
+
+    void GetInput(){
+        xMove = Input.GetAxis("Horizontal");
+        yMove = Input.GetAxis("Vertical");
+    }
+
+    void Move(){
+		rb.velocity = new Vector2(xMove * moveMod, yMove * moveMod);
+    }
+}
