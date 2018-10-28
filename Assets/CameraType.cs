06bd0027f69f9f2f8e8a93c8f504e5c600fec24a
Austin <austin.laimos@mymail.champlain.edu>
2018-10-28 09:31:17 -0400

    Camera now zooms in and out

---
+++ Assets/CameraType.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraType : MonoBehaviour {
+
+	public enum CamType {ZoomIn, ZoomOut};
+	public CamType type = CamType.ZoomIn;
+}
