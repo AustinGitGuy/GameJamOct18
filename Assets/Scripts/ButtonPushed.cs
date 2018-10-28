edf16a42e26ca04f1636546a9ac3d2f65cc50685
Andrew <andrewjosephmac@gmail.com>
2018-10-28 19:55:40 -0400

    Push pull sound

--- Assets/Scripts/ButtonPushed.cs
+++ Assets/Scripts/ButtonPushed.cs
@@ -6,6 +6,7 @@ public class ButtonPushed : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D col){
 		if(col.gameObject.tag == "Skull"){
+            Managers.SoundManagerScript.Instance.playSkullOnBloodSound();
 			Managers.PlayerManager.Instance.SkullCollected(1);
 			Destroy(col.gameObject);
 		}
